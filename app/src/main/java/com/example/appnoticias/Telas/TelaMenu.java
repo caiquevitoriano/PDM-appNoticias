49e9f92f58d21a4a98042754261268e8f9cdddab
caiquevitoriano <vitorianocaique@gmail.com>
2019-07-15 19:41:12 -0300

    setando itens do menu nem xml

--- app/src/main/java/com/example/appnoticias/Telas/TelaMenu.java
+++ app/src/main/java/com/example/appnoticias/Telas/TelaMenu.java
@@ -2,14 +2,29 @@ package com.example.appnoticias.Telas;
 
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 import com.example.appnoticias.R;
 
 public class TelaMenu extends AppCompatActivity {
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.game_menu, menu);
+        MenuItem item1 = menu.add ("Dados");
+        MenuItem item2 = menu.add ("Sair");
+
+        item1.setOnMenuItemClickListener (new MenuItem.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick (MenuItem item){
+                return true;
+            }
+        });
+
+        item2.setOnMenuItemClickListener (new MenuItem.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick (MenuItem item){
+                return true;
+            }
+        });
         return true;
     }
 }
